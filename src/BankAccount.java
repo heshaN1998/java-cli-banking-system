a703b26be028fa4d406431b485e66e5f74554f31
heshan <heshansandakelum2018@gmail.com>
2026-04-06 06:25:33 +0530

    complete Account section with few logics

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -13,6 +13,11 @@ public class BankAccount {
         this.balance=0.0;
         this.transactions=new ArrayList<>();
     }
+    @Override
+    public String toString(){
+        return "Account No: "+accountNumber +" | Name: "+accountHolderName+" | Balance: "+balance;
+    }
+
 
     public String getAccountNumber(){
         return accountNumber;
@@ -42,5 +47,41 @@ public class BankAccount {
         transactions.add(new Transaction("WITHDRAW",amount,"amoun withdrawn"));
     }
 
+    public void transfer(BankAccount reciever,double amount){
+        if(reciever==null){
+            throw new IllegalArgumentException("Reciever amount not found");
+        }
+        if (reciever.getAccountNumber().equals(this.accountNumber)){
+            throw new IllegalArgumentException("Cannot transfer to the same account");
+        }
+        if (amount<=0){
+            throw new IllegalArgumentException("Transfer amount must be greater than zero");
+        }
+        if(amount>balance){
+            throw new IllegalArgumentException("Insufficient balance for transfer");
+        }
+        this.balance=this.balance-amount;
+        reciever.balance= reciever.balance+amount;
+
+        this.transactions.add(new Transaction(
+                "Transfer_out",amount,"Transfered to account"+reciever.getAccountNumber()
+
+        ));
+
+        reciever.transactions.add(new Transaction(
+                "Transfer_in",amount,"Recived from account"+this.accountNumber
+        ));
+    }
+public void printTransactionHistory(){
+        if (transactions.isEmpty()){
+            System.out.println("No transaction found");
+            return;
+        }
+    System.out.println("\n Transaction history for account: "+accountNumber);
+        for (Transaction transaction : transactions){
+            System.out.println(transaction);
+        }
+
+}
 
 }
